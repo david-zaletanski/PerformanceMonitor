690f934e832882845ddc68618ec081c313402216
David Zaletanski <david.zaletanski@gmail.com>
2017-08-26 10:40:01 -0500

    Removed test method.

--- src/main/java/net/dzale/diezel/controller/DiezelController.java
+++ src/main/java/net/dzale/diezel/controller/DiezelController.java
@@ -39,18 +39,6 @@ public class DiezelController {
         return "index";
     }
 
-    /**
-     * Test comment.
-     *
-     * @return
-     * @throws DiezelException
-     */
-    @RequestMapping("/admintest")
-    public String handleTestRequest() throws DiezelException {
-        log.debug("TEST ENDPOINT CALLED");
-        return "TEST SUCCESS";
-    }
-
     @RequestMapping("/admin")
     public String handleAdminRequest(Model model) throws DiezelException {
         model.addAttribute("recentSystemCpuLoad", systemMetricsService.getRecentMetricsSystemCPULoadDataPoints());
